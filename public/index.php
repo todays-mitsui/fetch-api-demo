7c0e59fcb1a58381acd29a437fcc477f6e6e4ed0
三井 翔吾 <mitsui@queserser-j.co.jp>
2016-04-20 06:54:32 +0900

    first commit.

---
+++ public/index.php
@@ -0,0 +1,46 @@
+<?php
+
+require_once(__DIR__."/../vendor/autoload.php");
+
+
+$app = new \Slim\App;
+
+$app->get("/", function ($request, $response) {
+    return $response->write("
+      <h1>JSON API</h1>
+      <ul>
+        <li><a href=\"/api/200\" target=\"_blank\">2xx 正常終了</a></li>
+        <li><a href=\"/api/400\" target=\"_blank\">4xx クライアントエラー</a></li>
+        <li><a href=\"/api/500\" target=\"_blank\">5xx サーバーエラー</a></li>
+      </ul>
+    ");
+});
+
+$app->get("/api/200", function ($request, $response) {
+    $data = [
+        "status"  => "OK",
+        "message" => "リクエストは正常に処理されました",
+    ];
+
+    return $response->withJson($data, 200, JSON_PRETTY_PRINT|JSON_UNESCAPED_UNICODE);
+});
+
+$app->get("/api/400", function ($request, $response) {
+    $data = [
+        "status"  => "Bad Request",
+        "message" => "不正なリクエストです",
+    ];
+
+    return $response->withJson($data, 400, JSON_PRETTY_PRINT|JSON_UNESCAPED_UNICODE);
+});
+
+$app->get("/api/500", function ($request, $response) {
+    $data = [
+        "status"  => "Internal Server Error",
+        "message" => "リクエストの処理中にエラーが起きました",
+    ];
+
+    return $response->withJson($data, 500, JSON_PRETTY_PRINT|JSON_UNESCAPED_UNICODE);
+});
+
+$app->run();
